2a41e5d87dfae3af94f34a4af451ccbde8c56339
Sydney Reid <sydneyreid@Sydneys-MacBook-Air.local>
2026-07-12 10:45:03 -0400

    Added songs API route

--- app.js
+++ app.js
@@ -13,6 +13,27 @@ app.get('/goodbye', (req, res) => {
     res.send('Goodbye Express');
 });
 
+app.get('/songs', (req, res) => {
+
+    const songs = [
+        {
+            title: "Blinding Lights",
+            artist: "The Weeknd"
+        },
+        {
+            title: "Flowers",
+            artist: "Miley Cyrus"
+        },
+        {
+            title: "Shape of You",
+            artist: "Ed Sheeran"
+        }
+    ];
+
+    res.json(songs);
+
+});
+
 app.listen(3000, () => {
     console.log('Listening on port 3000');
 });
